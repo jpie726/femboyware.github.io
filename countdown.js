dc71bb118ab7d24a42b926c1fd198f45545e4f2d
ChaxGay <69131569+ChaxGay@users.noreply.github.com>
2020-11-17 21:17:39 +0100

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,20 @@
+function updateTimer() {
+future = Date.parse("Nov 24, 2020 0:0:00");
+now = new Date();
+diff = future - now;
+
+days = Math.floor(diff / (1000 * 60 * 60 * 24));
+hours = Math.floor(diff / (1000 * 60 * 60));
+mins = Math.floor(diff / (1000 * 60));
+secs = Math.floor(diff / 1000);
+
+d = days;
+h = hours - days * 24;
+m = mins - hours * 60;
+s = secs - mins * 60;
+
+document.getElementById("timer")
+    .innerHTML =
+    '<div>' + d + '<span>days</span></div>' ;
+}
+setInterval('updateTimer()', 1000);
---
+++ index.php
@@ -0,0 +1,41 @@
+<DOCTYPEHtml>
+    <html>
+    <head>
+<link rel="stylesheet" href="fuckingcss.css">
+<link rel="stylesheet" href="background.css">
+<div id="output"></div>
+<!-- Load Babel -->
+<!-- v6 <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script> -->
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+<!-- Your custom script here -->
+<script type="text/babel" src="background.js"></script>
+ <script src="countdown.js"></script> 
+ <script>
+ </script>
+    <link rel="shortcut icon" type="image/jpg" href="/favicon.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> FemboyWare </title>
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="FemboyWare" />
+    <meta property="og:description" content="The Official FemboyWare Website!" />
+    <meta property="og:url" content="http://femboyware.tk/" />
+    <meta property="og:image" content="https://i.imgur.com/AQPhekE.png" />
+    </head>
+    <body>
+    <canvas></canvas>
+    <p id="two" class="glow"> Femboyware </p>
+    <div class="wrapper">
+        <p id="one"> Our Discord and Steam! </p>
+        <div class="grid-container">
+        <button style="margin:25px;" class="button" onclick="location.href = 'https://discord.gg/SFjAE2eV'"> </button1>
+        <button style="margin:25px;" class="button button2" onclick="location.href = 'https://steamcommunity.com/groups/OfficialFemboyWare'"> </button2>
+        </div>
+    <div id="timer"></div>
+    <p id="one"> Till the Release of the Beta. </p>
+     <audio autoplay loop>
+  <source src="background.ogg" type="audio/ogg">
+  Your browser does not support the audio tag.
+</audio> 
+    </div>
+    </body>
+    </html>
